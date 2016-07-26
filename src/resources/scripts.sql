e1c558ef4cebb54444c6d21ccda7a5fe0c7f6947
kashif <kashif@greytip.com>
2016-07-26 20:00:11 +0530

    Commit 6 : Add script files to create table and insert values

---
+++ src/resources/scripts.sql
@@ -0,0 +1,21 @@
+
+
+CREATE TABLE users (
+  USER_ID bigserial NOT NULL,
+  USERNAME character varying(40) NOT NULL,
+  PASSWORD character varying(40) NOT NULL,
+  ACTIVE SMALLINT NOT NULL,
+  constraint pk_users primary key (USER_ID)
+)
+
+CREATE TABLE user_roles (
+  USER_ROLE_ID bigserial NOT NULL,
+  USER_ID integer NOT NULL,
+  AUTHORITY character varying(45) NOT NULL,
+ constraint pk_user_roles primary key (USER_ROLE_ID),
+  CONSTRAINT FK_user_roles FOREIGN KEY (USER_ID) REFERENCES users (USER_ID)
+)
+
+INSERT INTO users  VALUES ('1', 'kashif',  'imran',  '1' );
+
+INSERT INTO user_roles VALUES ('1',  '1',  'ROLE_USER' );
